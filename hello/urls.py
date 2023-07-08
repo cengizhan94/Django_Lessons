a16649dba0fa0a01f6a2b9a6146190d9dbb5a9c2
Cengiz Han <sonsair11@gmail.com>
2023-07-08 15:59:45 +0300

    Hello, anyone!

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path("", views.index, name="index")
-    
+    path("", views.index, name="index"),
+    path("cengo", views.cengo, name="cengo"),
+    path("zeynep", views.zeynep, name="zeynep"),
+    path("<str:name>",views.greet, name="greet"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -6,3 +6,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, Django!")
+
+def cengo(request):
+    return HttpResponse("Hello,Cengo!")
+
+def zeynep(request):
+    return HttpResponse("Hello,Zeynep!")
+
+def greet(request,name):
+    return HttpResponse(f"Hello, {name}!")
