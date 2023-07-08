6edec2a37a4a8ef4a285f6642ec4510671a70790
Cengiz Han <sonsair11@gmail.com>
2023-07-08 18:36:09 +0300

    inheritance

--- lecture3/settings.py
+++ lecture3/settings.py
@@ -30,9 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ 
     'hello',
     'newyear',
+    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
